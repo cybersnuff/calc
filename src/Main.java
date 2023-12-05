44d2551b61299595eae0cb818a257ba23b7cabe9
Vadim <cybersnuff@yandex.ru>
2023-12-05 18:41:49 +0400

    calc version 1.04 - Исправлена уязвимость при которой, пользователь мог вводить числа выходящие из диапазона от 1 до 10 включительно

--- src/Main.java
+++ src/Main.java
@@ -4,61 +4,70 @@ import java.util.Scanner;
 public class Main {
     public static String calc() {
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input:");
-       String input = in.nextLine();
+                Scanner in = new Scanner(System.in);
+                System.out.println("Input:");
+                String input = in.nextLine();
 
-        String[] array = input.split(" ");
+                String[] array = input.split(" ");
 
-        int x = 0;
+                int x = 0;
 
-        if (array.length > 3) {
-            System.out.println("Output:");
-            throw new RuntimeException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        }
+                if (array.length > 3) {
+                    System.out.println("Output:");
+                    throw new RuntimeException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                }
 
-        try {
+                try {
 
-            int a = Integer.parseInt(array[0]);
-            int b = Integer.parseInt(array[2]);
+                    int a = Integer.parseInt(array[0]);
+                    int b = Integer.parseInt(array[2]);
 
-            if (array[1].equals("+")) {
-                x = a + b;
+                    if (a < 1 || a > 10 || b < 1 || b > 10) {
+                        System.out.println("Output:");
+                        throw new RuntimeException("Ваше число должно быть в диапазоне от 1 до 10 включительно");
+                    }
 
-            } else if (array[1].equals("-")) {
-                x = a - b;
 
-            } else if (array[1].equals("/")) {
-                x = a / b;
+                    if (array[1].equals("+")) {
+                        x = a + b;
 
-            } else if (array[1].equals("*")) {
-                x = a * b;
+                    } else if (array[1].equals("-")) {
+                        x = a - b;
 
-            } else {
+                    } else if (array[1].equals("/")) {
+                        x = a / b;
+
+                    } else if (array[1].equals("*")) {
+                        x = a * b;
+
+                    } else {
+                        System.out.println("Output:");
+                        throw new RuntimeException("неверный оператор, используйте только +, -, /, * " +
+                                "Ваш знак - " + array[1]);
+
+                    }
+
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Output:");
+                    throw new RuntimeException("Cтрока не является математической операцией");
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Output:");
+                    throw new RuntimeException("Используйте только целые числа");
+
+                }
+
+                String result = Integer.toString(x);
+
                 System.out.println("Output:");
-                throw new RuntimeException("неверный оператор, используйте только +, -, /, * " +
-                        "Ваш знак - " + array[1]);
+                System.out.println(result);
+
+                return result;
 
             }
 
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Output:");
-            throw new RuntimeException("Cтрока не является математической операцией");
 
-        } catch (NumberFormatException e) {
-            System.out.println("Output:");
-            throw new RuntimeException("Используйте только целые числа");
 
-        }
-
-        String result = Integer.toString(x);
-
-            System.out.println("Output:");
-            System.out.println(result);
-
-            return result;
-
-    }
     public static void main(String[] args) {
         calc();
 
