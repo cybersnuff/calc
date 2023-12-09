f0c2e9da705e5bf262607919a5ff210b1e7cfb1b
Vadim <cybersnuff@yandex.ru>
2023-12-09 20:44:17 +0400

    calc version 1.05b - Небольшое уточнение Exception'a связанным Exception'ом диапазона чисел.

--- src/Main.java
+++ src/Main.java
@@ -14,53 +14,56 @@ public class Main {
 
         if (array.length > 3) {
             System.out.println("Output:");
-                 throw new RuntimeException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                }
+            throw new RuntimeException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
 
-                try {
+        try {
 
-                    int a = Integer.parseInt(array[0]);
-                    int b = Integer.parseInt(array[2]);
+            int a = Integer.parseInt(array[0]);
+            int b = Integer.parseInt(array[2]);
 
-                    if (a < 1 || a > 10 || b < 1 || b > 10) {
-                        System.out.println("Output:");
-                        throw new RuntimeException("Ваше число должно быть в диапазоне от 1 до 10 включительно");
-                    }
-
-
-                    if (array[1].equals("+")) {
-                        x = a + b;
-
-                    } else if (array[1].equals("-")) {
-                        x = a - b;
-
-                    } else if (array[1].equals("/")) {
-                        x = a / b;
-
-                    } else if (array[1].equals("*")) {
-                        x = a * b;
-
-                    } else {
-                        System.out.println("Output:");
-                        throw new RuntimeException("неверный оператор, используйте только +, -, /, * " +
-                                "Ваш знак - " + array[1]);
-
-                    }
-
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("Output:");
-                    throw new RuntimeException("Cтрока не является математической операцией");
-
-                } catch (NumberFormatException e) {
-                    System.out.println("Output:");
-                    throw new RuntimeException("Используйте только целые числа");
-
-                }
-
-                String result = Integer.toString(x);
-
+            if (a < 1 || a > 10 || b < 1 || b > 10) {
                 System.out.println("Output:");
-                System.out.println(result);
+                throw new RuntimeException("Ваше число должно быть в диапазоне от 1 до 10 включительно. " +"Ваши числа: " + a + " и " + b);
+
+                }
+
+
+
+
+            if (array[1].equals("+")) {
+                x = a + b;
+
+            } else if (array[1].equals("-")) {
+                x = a - b;
+
+            } else if (array[1].equals("/")) {
+                x = a / b;
+
+            } else if (array[1].equals("*")) {
+                x = a * b;
+
+            } else {
+                System.out.println("Output:");
+                throw new RuntimeException("неверный оператор, используйте только +, -, /, * " +
+                        "Ваш знак - " + array[1]);
+
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Output:");
+            throw new RuntimeException("Cтрока не является математической операцией");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Output:");
+            throw new RuntimeException("Используйте только целые числа");
+
+        }
+
+        String result = Integer.toString(x);
+
+        System.out.println("Output:");
+        System.out.println(result);
 
         try {
             Thread.sleep(1000);
@@ -73,9 +76,9 @@ public class Main {
         System.out.println("calculator version 1.05a");
         System.out.println("========================");
 
-                return result;
+        return result;
 
-            }
+    }
 
     public static void main(String[] args) {
         calc();
